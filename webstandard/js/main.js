112a41417af844f7786bbba18c03afef74386d28
hodu97 <145514463+hodu97@users.noreply.github.com>
2023-10-19 17:33:47 +0900

    Add files via upload

---
+++ webstandard/js/main.js
@@ -0,0 +1,10 @@
+$('.tit .btn').click(function(){
+    //$('.nav').toggle();
+    //$('.nav').fadeToggle();
+    $('.nav').slideToggle();
+    $(this).toggleClass("on");
+});
+
+    $('.ban').slick({
+
+    });
